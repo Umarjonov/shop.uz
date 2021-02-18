3757ee0227bab298a2265c6bad86bb3b9eac03bd
Umarjonov <mr.usuliddin@mail.ru>
2021-02-18 10:09:45 +0500

    Admin Panel | Show Products in Home Page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $productAll = Product::latest()->get();
+        return view('index')->with(compact('productAll'));
     }
 }
